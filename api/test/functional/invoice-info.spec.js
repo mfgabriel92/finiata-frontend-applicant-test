66e2c495d801bd63a049fee6f1aa78c355f8d750
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-04-23 11:14:37 +0800

    Validation test improvement for adding recipient and invoice info (comparing JSONs)

--- api/test/functional/invoice-info.spec.js
+++ api/test/functional/invoice-info.spec.js
@@ -11,14 +11,19 @@ test("insertion of invoice info", async ({ client }) => {
     .send({
       invoiceId: 1,
       invoiceAmount: 123.45,
-      paymentTarget: moment().format('YYYY-MM-DD HH:mm:ss')
+      paymentTarget: moment().format('2018-01-01 12:12:12')
     })
     .end();
 
   request.assertStatus(200);
+  request.assertJSONSubset({
+    invoice_id: 1,
+    invoiceAmount: 123.45,
+    paymentTarget: "2018-01-01 12:12:12",
+  });
 });
 
-test("failure of insertion with no invoice ID", async ({ client }) => {
+test("failure of insertion with non existent invoice ID", async ({ client }) => {
   const request = await client.post("http://127.0.0.1:3333/api/v1/invoices-info")
     .header('accept', 'application/json')
     .send({
--- api/test/functional/recipient.spec.js
+++ api/test/functional/recipient.spec.js
@@ -17,4 +17,41 @@ test("insertion of a recipient", async ({ client }) => {
     .end();
 
   request.assertStatus(200);
+  request.assertJSONSubset({
+    invoice_id: 1,
+    name: "John",
+    surname: "Doe",
+    address: "123 Lorem Ipsum, DO",
+    phone: "555555555"
+  });
+});
+
+test("failure insertion with non existent invoice ID", async ({ client }) => {
+  const request = await client.post("http://127.0.0.1:3333/api/v1/recipients")
+    .header('accept', 'application/json')
+    .send({
+      invoiceId: 999,
+      name: "John",
+      surname: "Doe",
+      address: "123 Lorem Ipsum, DO",
+      phone: "555555555"
+    })
+    .end();
+
+  request.assertStatus(404);
+});
+
+test("failure insertion without all fields", async ({ client }) => {
+  const request = await client.post("http://127.0.0.1:3333/api/v1/recipients")
+    .header('accept', 'application/json')
+    .send({
+      invoiceId: 1,
+      name: "John",
+      surname: "Doe",
+      address: "123 Lorem Ipsum, DO",
+      phone: null
+    })
+    .end();
+
+  request.assertStatus(500);
 });
